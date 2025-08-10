bda910ab9d081db70f957ccc9ab5b7fcfc349c4e
Filip Klimeš <filip@filipklimes.cz>
2025-08-10 17:23:02 +0200

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
+import { Disclosure } from '@headlessui/react';
+
+const navigation = [
+  { name: 'Chemoterapie', href: '#', current: true },
+  { name: 'Biopsie', href: '#', current: false },
+  { name: 'Brachytherapie', href: '#', current: false },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
 function App() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold underline">OnkoNote</h1>
+    <div className="min-h-full">
+      <Disclosure as="nav" className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <div className="flex items-baseline space-x-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? 'page' : undefined}
+                    className={classNames(
+                      item.current
+                        ? 'bg-gray-900 text-white'
+                        : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                      'rounded-md px-3 py-2 text-sm font-medium'
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Disclosure>
+
+      <header className="relative bg-white shadow-sm">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Chemoterapie
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {/* Your content */}
+        </div>
+      </main>
     </div>
   );
 }
