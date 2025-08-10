384a94f052e8f6af2afb8fd5e43a1a5c2177e37a
Filip Klimeš <filip@filipklimes.cz>
2025-08-10 16:53:34 +0200

    Add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,6 @@ function App() {
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl font-bold underline">OnkoNote</h1>
-      <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-1">
-          <h2 className="text-2xl font-bold">Note 1</h2>
-        </div>
-      </div>
     </div>
   );
 }
